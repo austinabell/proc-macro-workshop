e265df6a99ab39e49e241a20e89493c65519cd03
austinabell <austinabell8@gmail.com>
2019-12-08 11:20:07 -0500

    b2 pass

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,12 +2,26 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    eprintln!("INPUT: {:#?}", input);
+    let name = &input.ident;
+    let b_name = format!("{}Builder", name);
+    let b_ident = syn::Ident::new(&b_name, name.span());
 
-    TokenStream::new()
+    let expanded = quote! {
+        struct #b_ident {
+        }
+        impl #name {
+            fn builder() -> #b_ident {
+                #b_ident {
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
