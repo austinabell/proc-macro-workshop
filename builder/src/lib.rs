a5d0f2efa51e8aa392c46f1de0918f50012c383c
austinabell <austinabell8@gmail.com>
2019-12-08 17:46:07 -0500

    finish functionality

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,9 +4,9 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-fn ty_is_option(ty: &syn::Type) -> Option<&syn::Type> {
+fn ty_inner_type<'a>(wrapper: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> {
     if let syn::Type::Path(ref p) = ty {
-        if !(p.path.segments.len() == 1 && p.path.segments[0].ident == "Option") {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != wrapper {
             return None;
         }
 
@@ -24,7 +24,7 @@ fn ty_is_option(ty: &syn::Type) -> Option<&syn::Type> {
     None
 }
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let struct_name = &input.ident;
@@ -38,80 +38,163 @@ pub fn derive(input: TokenStream) -> TokenStream {
     {
         named
     } else {
-        unimplemented!()
+        unimplemented!();
     };
 
-    let optionized = fields.iter().map(|f| {
+    let builder_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if ty_is_option(ty).is_some() {
+        if ty_inner_type("Option", ty).is_some() || builder_of(&f).is_some() {
             quote! { #name: #ty }
         } else {
-            quote! {
-                #name: std::option::Option<#ty>
-            }
+            quote! { #name: std::option::Option<#ty> }
         }
     });
 
     let methods = fields.iter().map(|f| {
-        let name = &f.ident;
+        let name = f.ident.as_ref().unwrap();
         let ty = &f.ty;
-        if let Some(inner_ty) = ty_is_option(ty) {
+
+        let (arg_type, value) = if let Some(inner_ty) = ty_inner_type("Option", ty) {
+            (inner_ty, quote! { std::option::Option::Some(#name) })
+        } else if builder_of(&f).is_some() {
+            (ty, quote! { #name })
+        } else {
+            (ty, quote! { std::option::Option::Some(#name) })
+        };
+        let set_method = quote! {
+            pub fn #name(&mut self, #name: #arg_type) -> &mut Self {
+                self.#name = #value;
+                self
+            }
+        };
+
+        match extend_method(&f) {
+            None => set_method.into(),
+            Some((true, extend_method)) => extend_method,
+            Some((false, extend_method)) => {
+                // safe to generate both!
+                let expr = quote! {
+                    #set_method
+                    #extend_method
+                };
+                expr.into()
+            }
+        }
+    });
+
+    // for when you call Builder::build
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        if ty_inner_type("Option", &f.ty).is_some() || builder_of(f).is_some() {
             quote! {
-                pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
-                    self.#name = Some(#name);
-                    self
-                }
+                #name: self.#name.clone()
             }
         } else {
             quote! {
-                pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                    self.#name = Some(#name);
-                    self
-                }
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
             }
         }
     });
 
-    let build_fields = fields.iter().map(|f| {
+    let build_empty = fields.iter().map(|f| {
         let name = &f.ident;
-        if ty_is_option(&f.ty).is_some() {
-            return quote! {
-                #name: self.#name.clone()
-            };
-        }
-        quote! {
-            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
-        }
-    });
-
-    let builder_names = fields.iter().map(|f| {
-        let name = &f.ident;
-        quote! {
-            #name: None
+        if builder_of(f).is_some() {
+            quote! { #name: std::vec::Vec::new() }
+        } else {
+            quote! { #name: std::option::Option::None }
         }
     });
 
     let expanded = quote! {
-        struct #b_ident {
-            #(#optionized,)*
+        pub struct #b_ident {
+            #(#builder_fields,)*
         }
         impl #b_ident {
             #(#methods)*
-            pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
-                Ok(#struct_name {
+
+            pub fn build(&self) -> std::result::Result<#struct_name, std::boxed::Box<dyn std::error::Error>> {
+                std::result::Result::Ok(#struct_name {
                     #(#build_fields,)*
                 })
             }
         }
         impl #struct_name {
-            fn builder() -> #b_ident {
+            pub fn builder() -> #b_ident {
                 #b_ident {
-                    #(#builder_names,)*
+                    #(#build_empty,)*
                 }
             }
         }
     };
 
     expanded.into()
+}
+
+fn builder_of(f: &syn::Field) -> Option<&syn::Attribute> {
+    for attr in &f.attrs {
+        if attr.path.segments.len() == 1 && attr.path.segments[0].ident == "builder" {
+            return Some(attr);
+        }
+    }
+    None
+}
+
+fn extend_method(f: &syn::Field) -> Option<(bool, proc_macro2::TokenStream)> {
+    let name = f.ident.as_ref().unwrap();
+    let g = builder_of(f)?;
+
+    fn mk_err<T: quote::ToTokens>(t: T) -> Option<(bool, proc_macro2::TokenStream)> {
+        Some((
+            false,
+            syn::Error::new_spanned(t, "expected `builder(each = \"...\")`").to_compile_error(),
+        ))
+    }
+
+    let meta = match g.parse_meta() {
+        Ok(syn::Meta::List(mut nvs)) => {
+            // list here is .. in #[builder(..)]
+            assert_eq!(nvs.path.segments[0].ident, "builder");
+            if nvs.nested.len() != 1 {
+                return mk_err(nvs);
+            }
+
+            // nvs.nested[0] here is (hopefully): each = "foo"
+            match nvs.nested.pop().unwrap().into_value() {
+                syn::NestedMeta::Meta(syn::Meta::NameValue(nv)) => {
+                    if nv.path.segments[0].ident != "each" {
+                        return mk_err(nvs);
+                    }
+                    nv
+                }
+                meta => {
+                    // nvs.nested[0] was not k = v
+                    return mk_err(meta);
+                }
+            }
+        }
+        Ok(meta) => {
+            // inside of #[] there was either just an identifier (`#[builder]`) or a key-value
+            // mapping (`#[builder = "foo"]`), neither of which are okay.
+            return mk_err(meta);
+        }
+        Err(e) => {
+            return Some((false, e.to_compile_error()));
+        }
+    };
+
+    match meta.lit {
+        syn::Lit::Str(s) => {
+            let arg = syn::Ident::new(&s.value(), s.span());
+            let inner_ty = ty_inner_type("Vec", &f.ty).unwrap();
+            let method = quote! {
+                pub fn #arg(&mut self, #arg: #inner_ty) -> &mut Self {
+                    self.#name.push(#arg);
+                    self
+                }
+            };
+            Some((&arg == name, method))
+        }
+        lit => panic!("expected string, found {:?}", lit),
+    }
 }
