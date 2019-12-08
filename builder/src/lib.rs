2ffe4c262dae57f8122a4a3817a578fa4c1f8b1a
austinabell <austinabell8@gmail.com>
2019-12-08 11:47:43 -0500

    b3 pass

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,8 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
 use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -14,10 +14,36 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         struct #b_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #b_ident {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
         }
         impl #name {
             fn builder() -> #b_ident {
                 #b_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
                 }
             }
         }
